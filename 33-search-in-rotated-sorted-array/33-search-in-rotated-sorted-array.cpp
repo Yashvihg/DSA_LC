2a933bc441ddb5298505bdeac59e6195c0d182cd
Yashvi Himanshu Gaglani <67501898+Yashvihg@users.noreply.github.com>
2022-01-14 13:05:59 +0400

    Time: 3 ms (77.70%), Space: 11.2 MB (28.17%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int start = 0, end = int(nums.size())-1, mid; 
-        while(start<=end){
-            mid=start + end-start/2;
+        int n = nums.size();
+        int s=0, e = n-1;
+        int index = smallestElementIndex(nums, s, e, n);
+        int val = BS(nums, index, e, target);
+        if(val != -1) {return val;}
+        else {return BS(nums, 0, index-1, target);}
+        
+    }
+    
+    int smallestElementIndex(vector<int> &nums, int s, int e, int n){
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            int next=(mid+1)%n;
+            int prev=(mid+n-1)%n;
+
+            if((nums[mid] <= nums[next]) && (nums[mid] <= nums[prev]))
+                return mid;   
+            else 
+            {
+                if(nums[mid]>=nums[s] && nums[mid]<=nums[e]) { return s; }
+                else if(nums[s]<=nums[mid])
+                    s=mid+1;
+                else if(nums[mid]<=nums[e])
+                    e=mid-1;
+            }
+        }
+        return 0;
+    }
+    
+    int BS(vector<int> &nums, int s, int e, int target){
+        while(s<=e){
+            int mid = s + (e-s)/2;
             if(nums[mid] == target)
                 return mid; 
-            if(nums[start]<=nums[mid]){
-                if(target<=nums[mid] && target>=nums[start])
-                    end = mid - 1;
-                else start = mid + 1;
-            }
-            else //if(nums[mid]<=nums[end])
-                if(target>=nums[mid] && target<=nums[end])
-                    start = mid + 1;
-                else end = mid - 1; 
-            
+            else if(nums[mid]>target)
+                e=mid-1;
+            else if(nums[mid]<target)
+                s=mid+1;
         }
-        return -1;
+    return -1;
     }
 };
