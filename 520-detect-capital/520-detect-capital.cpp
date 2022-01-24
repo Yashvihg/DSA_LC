b23e484b5291499feedfdb362a8eaf5ee79cc0b8
Yashvi Himanshu Gaglani <67501898+Yashvihg@users.noreply.github.com>
2022-01-24 16:29:40 +0400

    Time: 3 ms (39.46%), Space: 5.9 MB (99.14%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int n = word.length();
-
+        
         int uppercount=0, lowercount=0;
-        if(n<=1){return true;}
-          for(int i=0; i<n; i++){
-               if(islower(word[i]))   
-                lowercount++;
-              else if(isupper(word[i]))   
+        // lowercount=0;
+        if(word.size()<=1){return true;}
+          for(int i=0; i<word.size(); i++){
+               if(isupper(word[i]))   
                 uppercount++;
+              else lowercount++;
           }  
         
-          if(lowercount == n) return true;
-          else if(uppercount == n) return true;
-          else if(isupper(word[0]) && lowercount == n-1) return true;
+          if((lowercount == word.size() || uppercount == word.size()) || (isupper(word[0]) && lowercount == word.size()-1)) return true;
           else return false;
 
     };
