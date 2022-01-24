aba24a9af5aba7c46566d582344572b40779c93a
Yashvi Himanshu Gaglani <67501898+Yashvihg@users.noreply.github.com>
2022-01-24 16:36:09 +0400

    Time: 0 ms (100.00%), Space: 6.2 MB (19.00%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -3,7 +3,6 @@ public:
     bool detectCapitalUse(string word) {
         
         int uppercount=0;
-        // if(word.size()<=1){return true;}
           for(int i=0; i<word.size(); i++){
                if(isupper(word[i]))   
                 uppercount++;
