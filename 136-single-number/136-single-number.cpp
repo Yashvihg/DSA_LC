587f7fa2de59fc8c232bb83223e1a7ef4e333020
Yashvi Himanshu Gaglani <67501898+Yashvihg@users.noreply.github.com>
2022-01-24 19:14:39 +0400

    Time: 25 ms (47.14%), Space: 16.9 MB (85.03%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -3,7 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         int value=0;
         for(int i=0; i<nums.size(); i++)
-            value = nums[i]^value;
+            value ^= nums[i];
         
         return value;
     }
