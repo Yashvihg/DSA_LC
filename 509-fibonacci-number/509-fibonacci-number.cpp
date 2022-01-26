9b3461ea73a17b9a289273b203f520ddfad9f746
Yashvi Himanshu Gaglani <67501898+Yashvihg@users.noreply.github.com>
2022-01-26 09:04:19 +0400

    Time: 0 ms (100.00%), Space: 6 MB (39.32%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<2) return n;
-        return fib(n-1) + fib(n-2);
+        // if(n<2) return n;
+        // return fib(n-1) + fib(n-2);
+        int a1=0, a2=1, sum=0;
+        for(int i=0; i<n; i++){
+            sum = a1+ a2;
+            a1=a2;
+            a2=sum;
+        }
+    return a1;
     }
 };
