f8aa94698dc1e38fedcc0e6b12050477d5e2cc9e
Yashvi Himanshu Gaglani <67501898+Yashvihg@users.noreply.github.com>
2022-02-05 15:27:40 +0400

    Time: 6 ms (53.32%), Space: 6 MB (24.53%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        long reqSum, mid;
-        long s=1, e=n, res;
+        long long reqSum, mid;
+        long long s=1, e=n, res;
         while(s<=e){
-            mid = s+(e-s)/2;  //rows
+            mid = s+(e-s)/2;
             reqSum =  (mid*(mid+1))/2;
             if(reqSum==n) return mid;
             if(reqSum<=n)
