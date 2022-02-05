81dcccfda366e0005087c65b74e055518d74db9e
Yashvi Himanshu Gaglani <67501898+Yashvihg@users.noreply.github.com>
2022-02-05 16:31:05 +0400

    Time: 4 ms (55.40%), Space: 6 MB (25.58%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -3,13 +3,16 @@ public:
     int mySqrt(int x) {
         if (x == 0 || x == 1)
         return x;
-        long i = 1;
-        long result = 1;
-        while (result <= x)
-        {
-          i++;
-          result = i * i;
+        int s=0, e=x, res;
+        while(s<=e){
+            long mid=s+(e-s)/2;
+            if(mid*mid == x) return mid;
+            else if(mid*mid < x){
+                s=mid+1;
+                res=mid;
+            }
+            else e=mid-1;
         }
-        return i - 1;
+        return res;
     }
 };
