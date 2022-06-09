0de70f6c4b2fec59fbbeb24f85208473ed4b99c0
Yashvi Himanshu Gaglani <67501898+Yashvihg@users.noreply.github.com>
2022-06-09 18:34:29 +0530

    Time: 20 ms (44.46%), Space: 15.6 MB (75.27%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -4,6 +4,7 @@ public:
         int s=0, e=numbers.size()-1;
         while(s<=e){
             int mid = s+(e-s)/2;
+            // if(target < numbers[mid]) e = mid;
             int sum = numbers[s] + numbers[e];
             if(sum == target) return {s+1, e+1};
             else if(sum>target) e--;
